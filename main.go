dac507122cd3785c845e9f149047d70d5510cd62
Stephen Smith <smistephen@users.noreply.github.com>
2025-08-29 23:11:51 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	env := os.Getenv("ENV")
+
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+			fmt.Fprintf(w, "Hello! I am running in %s\n", env)
+		})
+
+		log.Println("starting server on port :80")
+		if err := http.ListenAndServe(":80", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Hello! I am running in %s\n", env)
+	})
+
+	log.Println("starting server on port :443")
+	if err := http.ListenAndServeTLS(":443","crtfile.crt","keyfile.key", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
